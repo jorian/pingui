b9a5b3cae64eb79bc111337b0f24616ab6a7b779
jorian@outlook.com <jorian@outlook.com>
2018-01-06 21:43:59 +0100

    optimized buy command

--- src/main/java/controller/Trade.java
+++ src/main/java/controller/Trade.java
@@ -1,15 +1,12 @@
 package controller;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 import com.sun.javafx.scene.control.skin.TableHeaderRow;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import utils.BarterRPC;
@@ -20,6 +17,7 @@ import java.util.Arrays;
 
 public class Trade {
     private BarterRPC barterRPC;
+    private Pending pendingSwap;
 
     @FXML private GridPane contentPane;
     @FXML private TableView<Orders.Asks> orderbookAsks;
@@ -31,6 +29,7 @@ public class Trade {
     @FXML private TextField baseCoin;
     @FXML private TextField buyAmount;
     @FXML private TextField buyPrice;
+    @FXML private Label buyTotal;
 
 
     public Trade() throws Exception {
@@ -61,6 +60,23 @@ public class Trade {
             header.reorderingProperty().addListener((observable, oldValue, newValue) -> header.setReordering(false));
         });
 
+        buyTotal.setDisable(true);
+
+
+        buyAmount.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.equals("") && !buyPrice.getText().equals("")) {
+                double dBuyPrice = Double.valueOf(buyPrice.getText());
+                buyTotal.setText(String.valueOf(Double.valueOf(newValue) * dBuyPrice));
+            }
+        });
+
+        buyPrice.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.equals("") && !buyAmount.getText().equals("")) {
+                double dBuyAmount = Double.valueOf(buyAmount.getText());
+                buyTotal.setText(String.valueOf(Double.valueOf(newValue) * dBuyAmount));
+            }
+        });
+
 
         // DEBUG
         baseCoin.appendText("CRYPTO");
@@ -70,22 +86,25 @@ public class Trade {
     public void getOrderbook(Event e) throws Exception {
         String response = barterRPC.orderbook(baseCoin.getText(),relCoin.getText());
 
-        Orders orders = new Gson().fromJson(response, Orders.class);
+        // If something goes wrong with the request, the response will be empty.
+        if (!response.equals("")) {
+            Orders orders = new Gson().fromJson(response, Orders.class);
 
-        System.out.println(orders.asks.length);
-        System.out.println(orders.bids.length);
+            System.out.println(orders.asks.length);
+            System.out.println(orders.bids.length);
 
-        ObservableList<Orders.Asks> asksList = FXCollections.observableList(Arrays.asList(orders.asks));
-        ObservableList<Orders.Bids> bidsList = FXCollections.observableList(Arrays.asList(orders.bids));
+            ObservableList<Orders.Asks> asksList = FXCollections.observableList(Arrays.asList(orders.asks));
+            ObservableList<Orders.Bids> bidsList = FXCollections.observableList(Arrays.asList(orders.bids));
 
-        orderbookAsks.getItems().clear();
-        orderbookAsks.getItems().addAll(asksList);
+            orderbookAsks.getItems().clear();
+            orderbookAsks.getItems().addAll(asksList);
 
-        orderbookBids.getItems().clear();
-        orderbookBids.getItems().addAll(bidsList);
+            orderbookBids.getItems().clear();
+            orderbookBids.getItems().addAll(bidsList);
 
-        avgaskvolume.setText("Avg volume (" + orders.bids[0].coin + ")");
-        avgbidvolume.setText("Avg volume (" + orders.asks[0].coin + ")");
+            avgaskvolume.setText("Avg volume (" + orders.bids[0].coin + ")");
+            avgbidvolume.setText("Avg volume (" + orders.asks[0].coin + ")");
+        }
     }
 
     public static class Orders {
@@ -202,6 +221,136 @@ public class Trade {
 
     public void buyBtn(Event e) throws IOException {
         String response = barterRPC.buy(baseCoin.getText(),relCoin.getText(), Double.valueOf(buyAmount.getText()), Double.valueOf(buyPrice.getText()));
+
+        // If something goes wrong with the request, the response will be empty.
+        if (!response.equals("")) {
+            JsonObject jsonObject = new JsonParser().parse(response).getAsJsonObject();
+            if (jsonObject == null) {
+                System.out.println("json is NULL");
+            } else {
+                if (jsonObject.get("result").toString().equals("success")) {
+                    JsonObject jsonPending = jsonObject.getAsJsonObject("pending");
+
+                    pendingSwap = new Gson().fromJson(jsonPending, Pending.class);
+
+                    System.out.println("Expiration: " + pendingSwap.expiration);
+                }
+            }
+        }
+
+
+    }
+
+    public static class Pending {
+        private long expiration;
+        private int timeleft;
+        private long tradeid;
+        private long requestid;
+        private long quoteid;
+        private String bob;
+        private String base;
+        private double basevalue;
+        private String alice;
+        private String rel;
+        private double relvalue;
+        private long aliceid;
+
+
+        public long getExpiration() {
+            return expiration;
+        }
+
+        public void setExpiration(long expiration) {
+            this.expiration = expiration;
+        }
+
+        public int getTimeleft() {
+            return timeleft;
+        }
+
+        public void setTimeleft(int timeleft) {
+            this.timeleft = timeleft;
+        }
+
+        public long getTradeid() {
+            return tradeid;
+        }
+
+        public void setTradeid(long tradeid) {
+            this.tradeid = tradeid;
+        }
+
+        public long getRequestid() {
+            return requestid;
+        }
+
+        public void setRequestid(long requestid) {
+            this.requestid = requestid;
+        }
+
+        public long getQuoteid() {
+            return quoteid;
+        }
+
+        public void setQuoteid(long quoteid) {
+            this.quoteid = quoteid;
+        }
+
+        public String getBob() {
+            return bob;
+        }
+
+        public void setBob(String bob) {
+            this.bob = bob;
+        }
+
+        public String getBase() {
+            return base;
+        }
+
+        public void setBase(String base) {
+            this.base = base;
+        }
+
+        public double getBasevalue() {
+            return basevalue;
+        }
+
+        public void setBasevalue(double basevalue) {
+            this.basevalue = basevalue;
+        }
+
+        public String getAlice() {
+            return alice;
+        }
+
+        public void setAlice(String alice) {
+            this.alice = alice;
+        }
+
+        public String getRel() {
+            return rel;
+        }
+
+        public void setRel(String rel) {
+            this.rel = rel;
+        }
+
+        public double getRelvalue() {
+            return relvalue;
+        }
+
+        public void setRelvalue(double relvalue) {
+            this.relvalue = relvalue;
+        }
+
+        public long getAliceid() {
+            return aliceid;
+        }
+
+        public void setAliceid(long aliceid) {
+            this.aliceid = aliceid;
+        }
     }
 
     public static class BuyResponse {
@@ -209,9 +358,11 @@ public class Trade {
 
 
         public static class Swaps {
-            private double requestID;
-            private double quoteID;
 
+            public static class Swap {
+                private double requestID;
+                private double quoteID;
+            }
         }
 
         public static class NetAmounts {}
--- src/main/java/utils/BarterRPC.java
+++ src/main/java/utils/BarterRPC.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -37,39 +38,52 @@ public class BarterRPC {
                 "\"price\":" + price +
                 "}";
 
+        System.out.println("Method call: " + postJSONData);
+
         return postRequest(postJSONData);
     }
 
 
-    private String postRequest(String postJSONData) throws IOException {
-        connection = (HttpURLConnection) url.openConnection();
+    private String postRequest(String postJSONData) {
 
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-Type", "application/json");
-        connection.setDoOutput(true);
+        try {
+            connection = (HttpURLConnection) url.openConnection();
 
-        DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
 
-        dataOutputStream.writeBytes(postJSONData);
-        dataOutputStream.flush();
-        dataOutputStream.close();
+            DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
 
-        System.out.println(connection.getResponseCode());
+            dataOutputStream.writeBytes(postJSONData);
+            dataOutputStream.flush();
+            dataOutputStream.close();
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String output;
-        StringBuilder response = new StringBuilder();
+            System.out.println(connection.getResponseCode());
 
-        while ((output = in.readLine()) != null) {
-            response.append(output);
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String output;
+            StringBuilder response = new StringBuilder();
+
+            while ((output = in.readLine()) != null) {
+                response.append(output);
+            }
+
+            in.close();
+
+
+            System.out.println(response.toString());
+
+            connection.disconnect();
+
+            return response.toString();
+
+        } catch (ConnectException c) {
+            System.err.println("Connection refused. marketmaker might not be running");
+            return "";
+        } catch (Exception e) {
+            System.err.println("General exception");
+            return "";
         }
-
-        in.close();
-
-        System.out.println(response.toString());
-
-        connection.disconnect();
-
-        return response.toString();
     }
 }
